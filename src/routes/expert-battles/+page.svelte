9a58ed8ac1eb3b37d0e9afdecd1ae46d9a6b519a
yanqui-uxo <zdiesing@icloud.com>
2026-06-17 03:28:46 -0400

    Add image toggle

--- src/routes/expert-battles/+page.svelte
+++ src/routes/expert-battles/+page.svelte
@@ -2,6 +2,8 @@
 	import cards from '$lib/assets/expert_battles/cards.json';
 	import decks from '$lib/assets/expert_battles/decks.json';
 
+	const idsToNames = Object.fromEntries(cards.map((card) => [card.id, card.name]));
+
 	let search = $state('');
 	let searchTerms = $derived(search.split(',').map((search) => search.trim()));
 	let matchingCardArrays = $derived(
@@ -18,23 +20,36 @@
 			return matchingCardIdArrays.every((ids) => deckIds.some((id) => ids.includes(id)));
 		})
 	);
+
+	let showImages = $state(false);
 </script>
 
 <p>Search for cards, use commas between the cards to search for multiple</p>
 <input class="input" bind:value={search} />
 
+<input type="checkbox" id="showimages" class="checkbox" bind:checked={showImages} />
+<label for="showimages">Show images (WARNING: uses a lot of data)</label>
+
 {#each matchingDecks as deck (deck)}
 	<h1 class="text-2xl">{deck.name} ({deck.set})</h1>
-	<div class="flex">
-		{#each deck.cards as card (card.id)}
-			<div class="relative">
-				<img
-					src={`https://raw.githubusercontent.com/chase-mew/pokemon-tcg-pocket-cards/refs/heads/main/images/cards/${card.id}.png`}
-					class="w-24 h-auto"
-					alt={card.id}
-				/>
-				<p class="absolute z-10 bottom-0 right-0 bg-black">{card.count}</p>
-			</div>
-		{/each}
-	</div>
+	{#if showImages}
+		<div class="flex">
+			{#each deck.cards as card (card.id)}
+				<div class="relative">
+					<img
+						src={`https://raw.githubusercontent.com/chase-mew/pokemon-tcg-pocket-cards/refs/heads/main/images/cards/${card.id}.png`}
+						class="w-24 h-auto"
+						alt={card.id}
+					/>
+					<p class="absolute z-10 bottom-0 right-0 bg-black">{card.count}</p>
+				</div>
+			{/each}
+		</div>
+	{:else}
+		<ul>
+			{#each deck.cards as card (card.id)}
+				<li>{idsToNames[card.id]} x{card.count}</li>
+			{/each}
+		</ul>
+	{/if}
 {/each}
