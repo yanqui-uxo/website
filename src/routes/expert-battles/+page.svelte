dc9c42d37390a184e86e77b99269ce733a4c1c01
yanqui-uxo <zdiesing@icloud.com>
2026-06-17 20:51:38 -0400

    Make number font size larger

--- src/routes/expert-battles/+page.svelte
+++ src/routes/expert-battles/+page.svelte
@@ -41,7 +41,7 @@
 						class="w-24 h-auto"
 						alt={card.id}
 					/>
-					<p class="absolute z-10 bottom-0 right-0 bg-black rounded-full">{card.count}</p>
+					<p class="text-2xl absolute z-10 bottom-0 right-0 bg-black rounded-full">{card.count}</p>
 				</div>
 			{/each}
 		</div>
