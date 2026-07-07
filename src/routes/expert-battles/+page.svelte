4199fa7debae8b9bdb6eb66b83d135170abaf3a2
yanqui-uxo <zdiesing@icloud.com>
2026-07-07 13:05:58 -0400

    Use lazy-loading collapsible decks

--- src/routes/expert-battles/+page.svelte
+++ src/routes/expert-battles/+page.svelte
@@ -1,30 +1,17 @@
 <script lang="ts">
 	import cards from '$lib/assets/expert_battles/cards.json';
 	import decks from '$lib/assets/expert_battles/decks.json';
-	import type { Picture } from '@sveltejs/enhanced-img';
-	import { PersistedState } from 'runed';
 
-	const imports = import.meta.glob<Picture>('$lib/assets/expert_battles/images/*.avif', {
+	const imports = import.meta.glob<string>('$lib/assets/expert_battles/images/*.avif', {
 		eager: true,
-		import: 'default',
-		query: {
-			enhanced: true
-		}
+		import: 'default'
 	});
-
-	const idsToPictures = Object.fromEntries(
-		Object.entries(imports).map(([origPath, picture]) => [
+	const idsToImagePaths = Object.fromEntries(
+		Object.entries(imports).map(([origPath, path]) => [
 			origPath.replace(/.+\/(.+)\.avif/, '$1'),
-			picture
+			path
 		])
 	);
-	function getPicture(id: string): Picture {
-		const picture = idsToPictures[id];
-		if (!picture) {
-			throw new Error(`ID ${id} does not have associated picture`);
-		}
-		return picture;
-	}
 
 	const idsToNames = Object.fromEntries(cards.map((card) => [card.id, card.name]));
 
@@ -44,8 +31,6 @@
 			return matchingCardIdArrays.every((ids) => deckIds.some((id) => ids.includes(id)));
 		})
 	);
-
-	let showImages = new PersistedState('showImages', false);
 </script>
 
 <svelte:head>
@@ -55,33 +40,26 @@
 <p>Search for cards, use commas between the cards to search for multiple</p>
 <input class="input" bind:value={search} />
 
-<input type="checkbox" id="showimages" class="checkbox" bind:checked={showImages.current} />
-<label for="showimages"
-	>Show images (WARNING: may be slow on less powerful devices/connections)</label
->
-
 {#each matchingDecks as deck (deck)}
-	<br />
-
-	<h1 class="text-2xl">{deck.name} ({deck.set})</h1>
-	{#if showImages.current}
-		<div class="flex flex-wrap gap-1">
-			{#each deck.cards as card (card.id)}
-				<div class="relative">
-					<enhanced:img
-						src={getPicture(card.id)}
-						alt={`${idsToNames[card.id]} (${card.id})`}
-						class="w-24 h-auto"
-					/>
-					<p class="text-2xl absolute z-10 bottom-0 right-0 bg-black rounded-sm">{card.count}</p>
-				</div>
-			{/each}
+	<div class="collapse collapse-arrow">
+		<input type="checkbox" />
+		<h1 class="collapse-title text-2xl">{deck.name} ({deck.set})</h1>
+		<div class="collapse-content">
+			<div class="flex flex-wrap gap-1">
+				{#each deck.cards as card (card.id)}
+					<div class="relative">
+						<img
+							src={idsToImagePaths[card.id]}
+							alt={`${idsToNames[card.id]} (${card.id})`}
+							loading="lazy"
+							class="w-24 h-auto"
+						/>
+						<p class="right-0 bottom-0 z-10 absolute bg-black rounded-sm text-2xl">
+							{card.count}
+						</p>
+					</div>
+				{/each}
+			</div>
 		</div>
-	{:else}
-		<ul class="list-disc list-inside">
-			{#each deck.cards as card (card.id)}
-				<li>{idsToNames[card.id]} ({card.id}) x{card.count}</li>
-			{/each}
-		</ul>
-	{/if}
+	</div>
 {/each}
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import adapter from '@sveltejs/adapter-static';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { playwright } from '@vitest/browser-playwright';
@@ -8,7 +7,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
-		enhancedImages(),
 		sveltekit({
 			compilerOptions: {
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
