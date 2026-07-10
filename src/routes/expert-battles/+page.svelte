76bb418b7771de2fa4ee5cf3011b5d231e95e4c9
yanqui-uxo <zdiesing@icloud.com>
2026-07-10 01:34:14 -0400

    Make card images links

--- src/routes/expert-battles/+page.svelte
+++ src/routes/expert-battles/+page.svelte
@@ -56,25 +56,25 @@
 		<input type="checkbox" checked />
 		<h1 class="collapse-title text-2xl">{deck.name} ({deck.set})</h1>
 		<div class="collapse-content">
-			<div class="flex flex-wrap gap-1">
+			<div class="flex flex-wrap gap-2">
 				{#each deck.cards as card (card.id)}
-					<figure class="flex flex-col justify-end gap-1 w-24">
-						<figcaption class="text-xs text-center">
-							<a href={idToLink(card.id)} rel="external" class="link"
-								>{idsToNames[card.id]} <br /> ({card.id})</a
-							>
-						</figcaption>
-						<div class="relative">
-							<img
-								src={idsToImagePaths[card.id]}
-								alt={`${idsToNames[card.id]} (${card.id})`}
-								loading="lazy"
-							/>
-							<p class="right-0 bottom-0 z-10 absolute bg-black rounded-sm text-white text-2xl">
-								{card.count}
-							</p>
-						</div>
-					</figure>
+					<a href={idToLink(card.id)} rel="external" class="link">
+						<figure class="flex flex-col justify-end gap-1 w-24 h-full">
+							<figcaption class="text-xs text-center">
+								{idsToNames[card.id]} <br /> ({card.id})
+							</figcaption>
+							<div class="relative">
+								<img
+									src={idsToImagePaths[card.id]}
+									alt={`${idsToNames[card.id]} (${card.id})`}
+									loading="lazy"
+								/>
+								<p class="right-0 bottom-0 z-10 absolute bg-black rounded-sm text-white text-2xl">
+									{card.count}
+								</p>
+							</div>
+						</figure>
+					</a>
 				{/each}
 			</div>
 		</div>
