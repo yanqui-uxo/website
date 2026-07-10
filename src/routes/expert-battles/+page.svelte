0061bf160d4e94f4a9b6d8b2bfa9d16e46362a62
yanqui-uxo <zdiesing@icloud.com>
2026-07-10 01:40:00 -0400

    Remove redundant div

--- src/routes/expert-battles/+page.svelte
+++ src/routes/expert-battles/+page.svelte
@@ -55,28 +55,26 @@
 	<div class="collapse collapse-arrow">
 		<input type="checkbox" checked />
 		<h1 class="collapse-title text-2xl">{deck.name} ({deck.set})</h1>
-		<div class="collapse-content">
-			<div class="flex flex-wrap gap-2">
-				{#each deck.cards as card (card.id)}
-					<a href={idToLink(card.id)} rel="external" class="link">
-						<figure class="flex flex-col justify-end gap-1 w-24 h-full">
-							<figcaption class="text-xs text-center">
-								{idsToNames[card.id]} <br /> ({card.id})
-							</figcaption>
-							<div class="relative">
-								<img
-									src={idsToImagePaths[card.id]}
-									alt={`${idsToNames[card.id]} (${card.id})`}
-									loading="lazy"
-								/>
-								<p class="right-0 bottom-0 z-10 absolute bg-black rounded-sm text-white text-2xl">
-									{card.count}
-								</p>
-							</div>
-						</figure>
-					</a>
-				{/each}
-			</div>
+		<div class="collapse-content flex flex-wrap gap-2">
+			{#each deck.cards as card (card.id)}
+				<a href={idToLink(card.id)} rel="external" class="link">
+					<figure class="flex flex-col justify-end gap-1 w-24 h-full">
+						<figcaption class="text-xs text-center">
+							{idsToNames[card.id]} <br /> ({card.id})
+						</figcaption>
+						<div class="relative">
+							<img
+								src={idsToImagePaths[card.id]}
+								alt={`${idsToNames[card.id]} (${card.id})`}
+								loading="lazy"
+							/>
+							<p class="right-0 bottom-0 z-10 absolute bg-black rounded-sm text-white text-2xl">
+								{card.count}
+							</p>
+						</div>
+					</figure>
+				</a>
+			{/each}
 		</div>
 	</div>
 {/each}
